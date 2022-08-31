c3aeec111a3aec922c2ed9a552376e9719315f08
Heechan <ckstmznf@naver.com>
2022-09-01 00:23:58 +0900

    ChatPage layout

--- src/page/ChatPage.vue
+++ src/page/ChatPage.vue
@@ -1,6 +1,19 @@
 <template>
 <div class="page">
-  채팅 화면
+  <header>
+    <img src="@/assets/edcan.svg">
+    <h1>EDCAN Chat</h1>
+  </header>
+
+  <main>
+    <div class="chat-list">
+      asdas
+    </div>
+    <div class="chat-input-wrap">
+      <input type="text" placeholder="채팅을 입력하세요.">
+      <button>전송</button>
+    </div>
+  </main>
 </div>
 </template>
 
@@ -11,5 +24,58 @@ export default {
 </script>
 
 <style scoped>
+.page {
+  display: flex;
+}
+
+header {
+  height: 72px;
+  padding: 19px 12px;
+
+  background-color: #f00;
+
+  display: flex;
+  align-items: center;
+
+  position: fixed;
+  top : 0px;
+  left: 0px;
+  right: 0px;
+}
+
+header img {
+  width: 42px;
+}
+
+header h1 {
+  font-size: 27px;
+  font-weight: 500;
+
+  margin-left: 16px;
+
+  display: inline-block;
+}
+
+main {
+  flex : 1;
+
+  display: flex;
+  flex-direction: column;
+
+  padding: 72px 12px 16px;
+}
+
+.chat-list {
+  flex: 1;
+}
+
+.chat-input-wrap {
+  display: grid;
+  grid-template-columns: 1fr auto;
+  gap : 8px;
+}
+
+.chat-input-wrap button {
+}
 
 </style>
